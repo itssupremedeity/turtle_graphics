3a91406e947c1091026ccfa4fb50644a10b7df98
ndumthiy021 <ndumthiy021@student.wethinkcode.co.za>
2023-04-13 22:17:45 +0200

    Food object created

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -9,6 +9,10 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -17,3 +21,11 @@ screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
 game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+screen.exitonclick()
