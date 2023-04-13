4d5a8479f4c58b7d2dc062b4f326ff9288d9705b
ndumthiy021 <ndumthiy021@student.wethinkcode.co.za>
2023-04-13 22:24:50 +0200

    snake edits

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake_game import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -10,8 +11,6 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-scoreboard = Scoreboard()
-
 
 
 screen.listen()
@@ -27,5 +26,26 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+
+    # Detect collision with wall.
+    if (
+        snake.head.xcor() > 280
+        or snake.head.xcor() < -280
+        or snake.head.ycor() > 280
+        or snake.head.ycor() < -280
+    ):
+        game_is_on = False
+
+    # Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+
 
 screen.exitonclick()
