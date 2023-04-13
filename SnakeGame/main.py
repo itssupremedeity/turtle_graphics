f8e2551f3f27f6ed04ac70af8d844ba4c8d51a64
ndumthiy021 <ndumthiy021@student.wethinkcode.co.za>
2023-04-13 22:12:21 +0200

    snake object and main file created

---
+++ SnakeGame/main.py
@@ -0,0 +1,19 @@
+from turtle import Screen
+from snake_game import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
